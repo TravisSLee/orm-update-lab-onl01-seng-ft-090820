b88aaad5e70a5eac66d27dce81024cf94d27bb9c
Travis Lee <travisslee223@gmail.com>
2020-10-10 18:55:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,13 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-
+attr_accessor :name, :album
+attr_reader :id
+ 
+  def initialize(id=nil, name, album)
+    @id = id
+    @name = name
+    @album = album
+  end
 
 end
