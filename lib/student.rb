d5b037b656dc34e1bcf320b0ed40049dffb04b41
Travis Lee <travisslee223@gmail.com>
2020-10-10 19:06:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,12 +26,9 @@ class Student
   
   def self.drop_table
     sql = <<-SQL
-    CREATE TABLE students (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      grade INTEGER
-    )
+    DROP TABLE students
     SQL
+    DB[:conn].execute(sql)
   end
 
 
